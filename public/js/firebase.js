97d2f046ac7eec2822da3dcc85a0cd1dc711060f
Thiago Meireles <thiago22md@gmail.com>
2023-12-01 12:31:54 -0300

    debug

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
 let firebaseConfig = {
-    // Enter your firebase credentials
+    apiKey: "AIzaSyDkLa1L9mKIpjIKStlZCKRbb12OHP4P7vs",
+    authDomain: "blog-b1d8c.firebaseapp.com",
+    projectId: "blog-b1d8c",
+    storageBucket: "blog-b1d8c.appspot.com",
+    messagingSenderId: "1053242091185",
+    appId: "1:1053242091185:web:9d5fa52bdb9fed0fff2490",
+    measurementId: "G-MPS4KJXE2M"
 };
 
 firebase.initializeApp(firebaseConfig);
